9f786569dbf99cea510f2070fc690cc7c1f30b86
Alejandro Mejía <alejandroamm1432@gmail.com>
2024-05-28 21:40:01 -0500

    Products Management Finished

--- Server/app/routes/index.js
+++ Server/app/routes/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const usersRouter = require('./user'); // Importar subrutas de usuarios
+const productsRouter = require('./product'); // Importar subrutas de productos
 
 router.use('/users', usersRouter);
+
+router.use('/product', productsRouter);
 
 module.exports = router; 
--- Server/app/routes/product.js
+++ Server/app/routes/product.js
@@ -1,6 +1,7 @@
+// routes/products.js
 const express = require('express');
 const router = express.Router();
-const productController = require('../controller/productController'); // Importar el controlador de productos
+const productController = require('../controller/productController');
 
 router.get('/', productController.getAllProducts);
 router.post('/', productController.createProduct);
